559655e4f0935805f60c15e1021767d5edd222f6
Travis <test@test.com>
2016-07-04 18:57:55 -0700

    project restructure, moving more towards fp

---
+++ src/main/scala/LDSParser.scala
@@ -0,0 +1,66 @@
+/* LDSParser.scala
+ * Copyright (C) 2016  Travis Montoya
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.tgm.LDSReader
+
+import scala.xml.XML
+import org.htmlcleaner.HtmlCleaner
+import org.apache.commons.lang3.{StringEscapeUtils => Seu}
+import java.net.URL
+import java.io.File
+import sys.process._
+
+trait LDSReaderUtil {
+  val pl = "https://www.lds.org"
+  def updateDatabase(): Map[String,String]
+  def downloadPDF(url: String)
+}
+
+case class LDSParser() extends LDSReaderUtil {
+  private def savePDF(pdfLink: String) {
+    val pdfName = pdfLink.split("/")
+
+    println("Found PDF. Downloading to " + pdfName.last)
+    new URL(pdfLink) #> new File(pdfName.last) !!
+  }
+
+  def updateDatabase(): Map[String,String] = {
+    val url    = "https://www.lds.org/tools/rss?lang=eng&location=articles" +
+                 "&uri=/prophets-and-apostles/recent-messages"
+    val xml    = XML.load(url)
+    val items  = xml \\ "channel" \\ "item"
+    val titles = (items \\ "title").map(_.text.trim)
+    val links  = (items \\ "link").map(_.text.trim)
+
+    (titles zip links).toMap
+  }
+
+  def downloadPDF(url: String) {
+    val htmlCleaner = new HtmlCleaner
+    val cleanerProp = htmlCleaner.getProperties
+    val html        = htmlCleaner.clean(new URL(url))
+    val element     = html.getElementsByName("a", true)
+
+    element foreach { e =>
+      val linkTitle = e.getAttributeByName("title")
+      if (linkTitle != null && linkTitle.equalsIgnoreCase("PDF")) {
+        val pdfLink = Seu.unescapeHtml4(e.getAttributeByName("href").toString)
+
+        savePDF(pdfLink)
+      }
+    }
+  }
+}
--- src/main/scala/ldsreader.scala
+++ src/main/scala/ldsreader.scala
@@ -1,4 +1,4 @@
-/* ldsreader.scala
+/* LDSReader.scala
  * Copyright (C) 2016  Travis Montoya
  *
  * This program is free software: you can redistribute it and/or modify
@@ -14,53 +14,17 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package com.tgm.ldsreader
+package com.tgm.LDSReader
 
-import scala.xml.XML
 import scala.io.StdIn
-import org.htmlcleaner.HtmlCleaner
-import org.apache.commons.lang3.StringEscapeUtils
-import java.net.URL
-import java.io.File
-import sys.process._
+import LDSParser._
 
-object ldsreader {
-  def updateDatabase(): Map[String,String] = {
-    val url    = "https://www.lds.org/tools/rss?lang=eng&location=articles" +
-                 "&uri=/prophets-and-apostles/recent-messages"
-    val xml    = XML.load(url)
-    val items  = xml \\ "channel" \\ "item"
-    val titles = (items \\ "title").map(_.text.trim)
-    val links  = (items \\ "link").map(_.text.trim)
-
-    (titles zip links).toMap
-  }
-
-  def downloadPDF(url: String) {
-    val htmlCleaner = new HtmlCleaner
-    val cleanerProp = htmlCleaner.getProperties
-    val html        = htmlCleaner.clean(new URL(url))
-    val element     = html.getElementsByName("a", true)
-
-    for (elem <- element) {
-      val linkTitle = elem.getAttributeByName("title")
-      if (linkTitle != null && linkTitle.equalsIgnoreCase("PDF")) {
-        val pdfLink = StringEscapeUtils.unescapeHtml4(
-          elem.getAttributeByName("href").toString)
-        val pdfName = pdfLink.split("/")
-
-        println("Found PDF. Downloading to " + pdfName.last)
-        new URL(pdfLink) #> new File(pdfName.last) !!
-      }
-    }
-  }
-
+object LDSReader extends LDSParser {
   def main(args: Array[String]) {
     println("Search recent messages by the Prophet and Apostles")
     println("To download a talk paste the link into the search and press enter")
     print("Updating database! This can take a while...")
 
-    val pl = "https://www.lds.org"
     val db = updateDatabase()
 
     println("Done!")
